c71acef227b77ff643cedd63605ab7795f215df4
Pipemma <dremmaokoh@gmail.com>
2022-08-03 09:51:43 +0100

    New Code

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,8 @@
+
+
 const mongoose = require('mongoose');
 
+//Connecting to database
 const connectDB = async () => {
 await mongoose.connect(process.env.MONGODB_URL, () => {
     console.log ('Conneted to Mongosh');
